adcd0be4b609bc42aca5728c0af9880570c282a7
Sheldon Rampton <sheldon@sheldonrampton.com>
2014-02-24 09:42:15 -0800

    revisions to distro.pull.sh

--- scripts/distro.pull.sh
+++ scripts/distro.pull.sh
@@ -6,4 +6,7 @@ echo "Pulling in latest updates on branch $BRANCH from remote $GITURL."
  
 # Change to git root directory.
 cd "$(git rev-parse --show-toplevel)"
-git subtree pull --squash --prefix=$PREFIX $GITURL $BRANCH
+git checkout opencivic_branch
+git pull --rebase
+git checkout master
+git merge --squash -s subtree --no-commit opencivic_branch
