3325c8032b9b87419cbde969078f0274e47149f0
Sheldon Rampton <sheldon@sheldonrampton.com>
2014-02-25 10:17:33 -0800

    modify distro.pull.sh to derive URL for git pull from correct git origin remote

--- scripts/distro.pull.sh
+++ scripts/distro.pull.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-GITURL="https://github.com/civic-commons/opencivic.git"
+GITURL="$(git config --get remote.origin.url)"
 BRANCH="7.x-2.x"
 PREFIX="projects/opencivic"
 echo "Pulling in latest updates on branch $BRANCH from remote $GITURL."
